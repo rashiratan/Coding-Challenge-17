0846df4ce4331f21b0e61721f328e6130f7f56fb
Rashi Ratan <rashiiratan@gmail.com>
2024-11-23 17:33:09 -0500

    "Setup root component with global state and routing."

--- tour-comparison-app/src/App.jsx
+++ tour-comparison-app/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Gallery from './components/Gallery.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tours, setTours] = useState([]); //global state required in multiple components
+  const [visibleTours, setVisibleTours] = useState({}); //global state required in multiple components
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Gallery 
+        tours = {tours}
+        setTours = {setTours}
+        visibleTours = {visibleTours}
+        setVisibleTours = {setVisibleTours} //passing required states as props
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
 
-export default App
+export default App;
